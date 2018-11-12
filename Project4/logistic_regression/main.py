f608a53e9fb58d6426371032242df4c107bb337c
Alex Njoroge Waweru <njorogealexw@gmail.com>
2018-11-12 05:04:59 -0800

    Function level comments

--- Project4/logistic_regression/main.py
+++ Project4/logistic_regression/main.py
@@ -1,18 +1,56 @@
-
+import logging
 
 
 def logistic_reg(version, testfile):
-    print("Classifier: lr")
-    if (version == "u"):
-        print("Version: %s" % version)
-        
-        # Code for unnormalized this thing will go here
+    """
+    Args:
+        version (str): The version of the classifier to use
+        testfile (str): The txt file containing the sentiment data 
+    Raises:
+        IOError: Unable to read file
+    """
 
-    elif (version == "n"):
-        print("Version: %s" % version)
+    try:
+        print("Classifier: lr")
+        if (version == "u"):
+            print("Version: %s" % version)
+            
+            # Code for unnormalized this thing will go here
 
-        # Code for normalized this thing will go here
+        elif (version == "n"):
+            print("Version: %s" % version)
 
-    else:
-        print("Version not supported!!")
-        return
+            # Code for normalized this thing will go here
+
+        else:
+            print("Version not supported!!")
+            return
+    
+    except IOError:
+        logging.exception('') 
+    
+
+
+def input_conversion(testfile):
+    """
+    Args:
+        testfile (str): The txt file containing the sentiment data 
+    Returns:
+        input_list (list): A list of documents extracted from the testfile
+    Raises:
+        IOError: Unable to read file
+    """
+    
+    try:
+        # Initialize the input file as well as the input_list
+        inputfile = open(training_data_filename, 'r')
+        input_list = []
+
+        # Read each line in the input file and append it to the list
+        for line in fo.readlines():
+            input_list.append(line)
+        inputfile.close()
+        return input_list
+
+    except IOError:
+        logging.exception('') 
